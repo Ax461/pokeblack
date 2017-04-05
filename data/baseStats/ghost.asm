1c2d9761c2b3919c3075a9cfc2956173bcde18e6
Ax461 <Ax461@users.noreply.github.com>
2017-04-05 19:53:16 +0200

    Added ??? type

--- data/baseStats/ghost.asm
+++ data/baseStats/ghost.asm
@@ -4,8 +4,8 @@ db 100 ; base attack
 db 100 ; base defense
 db 100 ; base speed
 db 100 ; base special
-db GHOST_T ; species type 1
-db GHOST_T ; species type 2
+db UNKNOWN ; species type 1
+db UNKNOWN ; species type 2
 db 45 ; catch rate
 db 64 ; base exp yield
 INCBIN "pic/other/ghost.pic",0,1 ; 55, sprite dimensions
--- text/type_names.asm
+++ text/type_names.asm
@@ -29,6 +29,7 @@ TypeNames:
 	dw .Psychic
 	dw .Ice
 	dw .Dragon
+	dw .Unknown
 
 .Normal:   db "NORMAL@"
 .Fighting: db "FIGHTING@"
@@ -46,3 +47,4 @@ TypeNames:
 .Bug:      db "BUG@"
 .Ghost:    db "GHOST@"
 .Dragon:   db "DRAGON@"
+.Unknown:   db "???@"
