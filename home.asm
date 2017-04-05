cdbbafce34fb8d93a3db1e058de904f0493abd28
Ax461 <Ax461@users.noreply.github.com>
2017-04-05 19:59:47 +0200

    Replace jp [hl] with jp hl

--- home.asm
+++ home.asm
@@ -2970,7 +2970,7 @@ Bankswitch::
 	ld [MBC1RomBank],a
 	ld bc,.Return
 	push bc
-	jp [hl]
+	jp hl
 .Return
 	pop bc
 	ld a,b
@@ -4426,7 +4426,7 @@ CallFunctionInTable::
 	ld l, a
 	ld de, .returnAddress
 	push de
-	jp [hl]
+	jp hl
 .returnAddress
 	pop bc
 	pop de
@@ -4609,7 +4609,7 @@ CheckForHiddenObjectOrBookshelfOrCardKeyDoor::
 	ld [H_LOADEDROMBANK], a
 	ld de, .returnAddress
 	push de
-	jp [hl]
+	jp hl
 .returnAddress
 	xor a
 	jr .done
