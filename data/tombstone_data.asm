82944252f22def829cefecfce83cd18a15a98c35
Ax461 <Ax461@users.noreply.github.com>
2017-06-10 15:39:58 +0200

    More informative tombstone tile block list

--- data/tombstone_data.asm
+++ data/tombstone_data.asm
@@ -1,7 +1,7 @@
 TombstoneTileBlocks:
 	db $00
-	db $8A
-	db $84
+	db $8A ; CERULEAN_CITY
+	db $84 ; ROUTE_3
 	db $80
 	db $83
 	db $81
@@ -9,31 +9,31 @@ TombstoneTileBlocks:
 	db $83
 	db $85
 	db $86
-	db $81
-	db $87
+	db $81 ; ROUTE_4
+	db $87 ; ROUTE_24
 	db $86
 	db $89
 	db $88
 	db $89
 	db $88
 	db $89
-	db $80
+	db $80 ; VIRIDIAN_FOREST
 	db $80
 	db $81
-	db $74
+	db $74 ; PEWTER_GYM
 	db $75
-	db $81
+	db $81 ; MT_MOON_1
 	db $80
 	db $80
 	db $82
 	db $82
 	db $81
 	db $82
-	db $85
+	db $85 ; MT_MOON_3
 	db $84
 	db $84
 	db $83
 	db $83
-	db $76
+	db $76 ; CERULEAN_GYM
 	db $77
 	db $16
